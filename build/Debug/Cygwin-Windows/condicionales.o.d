fb80ddf63152ebc81b60d0cdb6f62bfe6dd902e0
carli <carli@LAPTOP-Q9M7PR5P>
2020-11-09 00:03:21 -0600

    commit

---
+++ build/Debug/Cygwin-Windows/condicionales.o.d
@@ -0,0 +1,1 @@
+build/Debug/Cygwin-Windows/condicionales.o: condicionales.c
---
+++ condicionales.c
@@ -0,0 +1,29 @@
+        
+
+//encabezados
+#include <stdio.h>
+#include <stdlib.h>
+// definicion y prototipo de funciones
+
+
+//variables y constantes
+
+
+
+//implementacion de funciones
+
+int main(int argc, char** argv) {
+    //declarar e imiciar variables
+    //variable (programacion]: espacio de memoria se manipula con un identidad (identificador]
+    //declarando una variable
+    float x=6;
+    float y=16;
+    
+   //bloque para el verdadero y probablemente falso 
+    if (x == y){
+        printf("son iguales");
+    }else{
+      printf("son Diferentes");  
+    }
+    return (0);
+}
